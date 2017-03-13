3d2bad9f05304df4a954f84b0fa077de2a521c75
Lucas Gabriel <lucasgabriel>
2017-03-13 17:53:25 -0300

    evolucao - App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
  <div>
   <h1 v-text="titulo"></h1>
-  <img :src="foto.url" :alt="foto.titulo"/>
+  <img v-for="foto in fotos" :src="foto.url" :alt="foto.titulo" class="foto-width"/>  
  </div>  
 </template>
 
@@ -10,16 +10,23 @@ export default {
   data() {
     return {
       titulo: 'AluraPic',
-      foto: {
+      fotos: [
+        {
          url: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRxmCGpcrjAq6DDT8hVt163lO1YulUCaunuv9Yp-BmE1IPb_P2g', 
          titulo: 'cachorro'
+       },
+      {
+         url:'https://s-media-cache-ak0.pinimg.com/736x/32/06/fb/3206fba0ed2a3d196460765e1f650905.jpg',
+         titulo: 'gato'
       }
+      ]       
     }
-  }
- 
+  } 
 }
 </script>
 
 <style>
-
+.foto-width {
+  width: 150px;
+}
 </style>
