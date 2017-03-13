0a1451e0c4ffa11637ada7c05d9c7747a6cb9f90
Lucas Gabriel <lucasgabriel>
2017-03-13 17:42:43 -0300

    git remote add origin https://github.com/lucasgabrielsa/alurapic.git

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+ <div>
+  <h1 v-text="titulo"></h1>
+  <img :src="foto.url" :alt="foto.titulo"/>
+ </div>  
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      titulo: 'AluraPic',
+      foto: {
+         url: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRxmCGpcrjAq6DDT8hVt163lO1YulUCaunuv9Yp-BmE1IPb_P2g', 
+         titulo: 'cachorro'
+      }
+    }
+  }
+ 
+}
+</script>
+
+<style>
+
+</style>
