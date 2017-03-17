4a5609abe52f536a883bf148474b21dc8670a0cc
Lucas Gabriel <lucasgabriel>
2017-03-17 16:17:05 -0300

    continuando aula 2 de vuejs

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,94 +1,109 @@
 <template>
     <div>
         <div class="content is-small">
-            <h1 v-text="titulo" class="texto-centralizado"></h1>
+            <h1 v-meu-transform.animate
+                v-text="titulo"
+                class="texto-centralizado"></h1>
         </div>
-        <input type="search" class="filtro Normal input" placeholder="Pesquise pelo Título" v-model="filtro"/>
+        <input type="search"
+               class="filtro Normal input"
+               placeholder="Pesquise pelo Título"
+               v-model="filtro" />
         <ul class="lista-fotos">
-            <li v-for="foto in fotosComFiltro" class="lista-fotos-item">      
-                    <meu-painel :titulo="foto.titulo">
-                        <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
-                        <!--<meu-botao tipo="button" rotulo="Remover" @click.native="removerFoto(foto)"></meu-botao>-->
-                        <meu-botao tipo="button" rotulo="Remover" :confirmacao="true" @botaoAtivado="removerFoto(foto, $event)" template="perigo"></meu-botao>
-                    </meu-painel>                 
+            <li v-for="foto in fotosComFiltro"
+                class="lista-fotos-item">
+                <meu-painel :titulo="foto.titulo">
+                    <imagem-responsiva v-meu-transform.animate.reverse="15"
+                                       :url="foto.url"
+                                       :titulo="foto.titulo" />
+                    <!--
+                    <imagem-responsiva v-meu-transform="{ incremento:15, animate: true }"
+                                       :url="foto.url"
+                                       :titulo="foto.titulo" /> -->
+                    <!--<meu-botao tipo="button" rotulo="Remover" @click.native="removerFoto(foto)"></meu-botao>-->
+                    <meu-botao tipo="button"
+                               rotulo="Remover"
+                               :confirmacao="true"
+                               @botaoAtivado="removerFoto(foto, $event)"
+                               template="perigo"></meu-botao>
+                </meu-painel>
             </li>
-        </ul>  
-    </div>  
+        </ul>
+    </div>
 </template>
 
 <script>
-    import Painel from '../shared/painel/Painel.vue';
-    import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue';
-    import Botao from '../shared/botao/Botao.vue';
+import Painel from '../shared/painel/Painel.vue';
+import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue';
+import Botao from '../shared/botao/Botao.vue';
 
-    export default {
+export default {
 
-        components: {
-            'meu-painel': Painel,
-            'imagem-responsiva': ImagemResponsiva, 
-            'meu-botao': Botao
-        },
+    components: {
+        'meu-painel': Painel,
+        'imagem-responsiva': ImagemResponsiva,
+        'meu-botao': Botao
+    },
 
-        data() {
-            return {
+    data() {
+        return {
             titulo: 'AluraPic',
-            fotos: [], 
-            filtro: ''       
-            }
-        },
+            fotos: [],
+            filtro: ''
+        }
+    },
 
-        methods: {
+    methods: {
 
-            pegarFotos() {
-                this.$http.get('http://localhost:3000/v1/fotos')    
+        pegarFotos() {
+            this.$http.get('http://localhost:3000/v1/fotos')
                 .then(res => res.json())
                 .then(data => this.fotos = data, erro => console.log(erro));
-            },
-        
-            removerFoto(foto, $event) {
-                console.log(foto.titulo + ' excluida com sucesso!');
-                console.log($event.msg);                
-            }
-                
-
         },
 
-        computed: {
-        
-            fotosComFiltro() {
-                if(this.filtro) {
-                    let exp = new RegExp(this.filtro.trim(), 'i');
-                    return this.fotos.filter(foto => exp.test(foto.titulo));
-                } else {
-                    return this.fotos;
-                }
-            }
+        removerFoto(foto, $event) {
+            console.log(foto.titulo + ' excluida com sucesso!');
+            console.log($event.msg);
+        }
 
-        },
-        
-        created() {
-            this.pegarFotos();
-        } 
-    
+
+    },
+
+    computed: {
+
+        fotosComFiltro() {
+            if (this.filtro) {
+                let exp = new RegExp(this.filtro.trim(), 'i');
+                return this.fotos.filter(foto => exp.test(foto.titulo));
+            } else {
+                return this.fotos;
+            }
+        }
+
+    },
+
+    created() {
+        this.pegarFotos();
     }
+
+}
 </script>
 
 <style scoped>
-
 .texto-centralizado {
-  text-align: center;
+    text-align: center;
 }
 
 .lista-fotos .lista-fotos-item {
-  display: inline-block;
+    display: inline-block;
 }
 
 ul {
-  list-style-type: none;
+    list-style-type: none;
 }
 
 .filtro {
-  display: block;
-  width: 100%;
+    display: block;
+    width: 100%;
 }
 </style>
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import Cadastro from './components/cadastro/Cadastro.vue';
 import Home from './components/home/Home.vue';
+import Relatorios from './components/relatorios/Relatorios.vue';
 
 export const routes = [
     { path:'', component: Home, name:'Home', id:1},
-    { path:'/cadastro', component:Cadastro, name:'Cadastro', id:2 }
+    { path:'/cadastro', component:Cadastro, name:'Cadastro', id:2 },
+    { path:'/relatorios', component:Relatorios, name:'Relatorios', id:3 }
 ];
